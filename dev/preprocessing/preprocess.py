8045b8c0b7c2000a8c1514585d300ef0d2824ad2
morsoletodev <106322393+morsoletodev@users.noreply.github.com>
2025-06-03 23:02:04 -0300

    feat: add preprocess module

---
+++ dev/preprocessing/preprocess.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+from dev.preprocessing.utils import *
+
+
+def preprocess ():
+    columns = ['RACACOR', 'ESCMAE2010', 'CODOCUPMAE',
+               'ESTCIVMAE', 'CONSULTAS', 'MESPRENAT',
+               'SEMAGESTAC', 'PESO', 'APGAR5', 'PARTO',
+               'IDANOMAL']
+
+    df = pd.read_parquet('../data/raw/DN2023.parquet.gzip',
+                         columns=columns)
+
+    df.dropna(inplace=True)
+
+    df = df[df['RACACOR'] != 3]
+    df = df[df['RACACOR'] != 5]
+
+    df = classify(df)
+
+    df = remove_outliers(df, 'PESO')
+    df = remove_outliers(df, 'SEMAGESTAC')
+
+    return df
---
+++ dev/preprocessing/utils.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import numpy as np
+
+
+def _class_ocup(x: int):
+    x = str(x)
+    if len(x) != 6:
+        return 0
+    elif x == '999991':  # Estudante
+        return 11
+    elif x == '999992':  # Dona de casa
+        return 12
+    else:
+        return int(x[:1])
+
+
+def classify (df: pd.DataFrame) -> pd.DataFrame:
+    map = {
+        1: 1,
+        2: 2,
+        4: 2
+    }
+
+    df['RACACOR'] = df['RACACOR'].map(map)
+
+    df['CODOCUPMAE'] = df['CODOCUPMAE'].apply(_class_ocup)
+
+    return df
+
+
+def remove_outliers (df, column):
+    # TODO: change to series
+    Q1 = df[column].quantile(0.25)
+    Q3 = df[column].quantile(0.75)
+    IQR = Q3 - Q1
+    lower = Q1 - 1.5*IQR
+    upper = Q3 + 1.5*IQR
+
+    upper_array = df.loc[df[column] > upper].index
+    lower_array = df.loc[df[column] < lower].index
+
+    df.drop(index=upper_array, inplace=True)
+    df.drop(index=lower_array, inplace=True)
+
+    return df
